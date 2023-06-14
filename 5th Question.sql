c1625e024723488a1bc3c01d32d9f093a38c6711
Mahmoud Hasan <mr.mahmoudhasan@gmail.com>
2023-06-14 14:34:24 +0200

    5th done

--- 5th Question.sql
+++ 5th Question.sql
@@ -3,4 +3,5 @@ FROM most_used_grapes_per_country
 INNER JOIN grapes
 ON grapes.id = most_used_grapes_per_country.grape_id
 GROUP by most_used_grapes_per_country.wines_count
-ORDER by Number_of_bottles DESC;
+ORDER by Number_of_bottles DESC
+limit 3;
