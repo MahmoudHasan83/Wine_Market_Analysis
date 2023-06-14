a84be24a505e961ebeaf2d02d2a9a010144a8fea
Mahmoud Hasan <mr.mahmoudhasan@gmail.com>
2023-06-14 14:24:45 +0200

    5th done

---
+++ 5th Question.sql
@@ -0,0 +1,6 @@
+SELECT grapes.name as Grape_name, SUM(most_used_grapes_per_country.wines_count) as Number_of_bottles
+FROM most_used_grapes_per_country
+INNER JOIN grapes
+ON grapes.id = most_used_grapes_per_country.grape_id
+GROUP by most_used_grapes_per_country.wines_count
+ORDER by Number_of_bottles DESC;
