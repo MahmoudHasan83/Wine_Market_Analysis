e209c325d98b8332e20808410bd5888f16a656f5
Mahmoud Hasan <mr.mahmoudhasan@gmail.com>
2023-06-14 15:45:45 +0200

    6th 2nd part is done

--- 6th Question.sql
+++ 6th Question.sql
@@ -6,3 +6,14 @@ INNER JOIN countries
 ON regions.country_code = countries.code
 GROUP BY countries.name
 ORDER BY Average_Rating DESC;
+
+SELECT countries.name AS Country, ROUND(AVG(vintages.ratings_average),3) AS Average_Rating
+FROM vintages
+INNER JOIN wines
+ON vintages.wine_id = wines.id
+INNER JOIN regions
+ON wines.region_id = regions.id
+INNER JOIN countries
+ON regions.country_code = countries.code
+GROUP BY countries.name
+ORDER BY Average_Rating DESC;
